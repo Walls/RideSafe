2aefdc2ad4bb4170a84cd6a3672bd39e390477a3
jemrrs <jemrrs@gmail.com>
2015-03-20 13:07:51 -0500

    Error landing page updates
    Added Facebook icon, better mobile scaling.

--- ridesafe.modev.systems/mapinterface.js
+++ ridesafe.modev.systems/mapinterface.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+        var map;  
+        var ref;
+    
         function goToError(){
             
             setTimeout(function(){
@@ -9,27 +12,14 @@ $(document).ready(function(){
             errorId.style.display = "block";
 
             var mapId = document.getElementById("map-canvas");
-            mapId.style.display = "none";   
+            mapId.style.display = "none"; 
+            
+            var splashId = document.getElementById("splash-wrapper");
+            slpashId.style.display = "none";
             
         }
-        var queryString = window.location.search;
-        queryString = queryString.substring(1);
-        if(queryString.length == 0){ 
-            goToError();
-        }
     
-        firebaseString = "https://dazzling-torch-5228.firebaseio.com/"+queryString;
-        var ref = new Firebase(firebaseString);
-        ref.once('value', function(snapshot) {
-        if (snapshot.val()==null) {
-            goToError();
-        }
-        
-        });
-    var map;
-    
-    //var mapId = document.getElementById("map-canvas");
-   // mapId.style.display = "none";
+
     function initialize(){
 
         // Get a reference to our posts
@@ -39,13 +29,14 @@ $(document).ready(function(){
 
         map = new google.maps.Map(document.getElementById('map-canvas'), {center: myLatlng, zoom: 17});
 
-            ref.on("child_added", function(snapshot) {
+            ref.child(queryString).on("child_added", function(snapshot) {
             var gpsCord = snapshot.val();
             //create Latlng maps var using gpsCord String
             var resStr = gpsCord.split(" ");
             var lat=Number(resStr[0]);
             var lng=Number(resStr[1]);
-
+            
+           
             //connect the dots
             prevLatlng = myLatlng;
             myLatlng = new google.maps.LatLng(lat,lng);
@@ -65,15 +56,51 @@ $(document).ready(function(){
 
             }
 
-        });
+            });
         
-    }
-    
-    google.maps.event.addDomListener(window, 'load', initialize());
-    
-    google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
-        $('body').addClass('fadeout');
-    });
+        }//end init
+        
+        //queryStrig extraction and firebase child check
+        var queryString = window.location.search;
+        queryString = queryString.substring(1);
+        if(queryString.length == 0){ 
+            goToError();
+        }
+        else{
+            ref = new Firebase("https://ridesafe.firebaseio.com/");
+            
+            ref.authAnonymously(function(error, authData) { 
+                 if (error) {
+                     window.alert("Authentication Error");
+                     goToError();
+                  } else {
+                    var count=0;
+                      
+                    ref.child(queryString).once('value', function(snapshot) {
+                    count++;
+                    if (snapshot.val() === null) {
+                        goToError();
+                    } 
+                    else {
+                        //if we got a good child, create a map and load it up with points, fadeout of loading screen
 
-    
-});
+                           google.maps.event.addDomListener(window, 'load', initialize());
+
+                           google.maps.event.addListenerOnce(map, 'tilesloaded', function(){ $('body').addClass('fadeout'); });
+                    }//end else
+
+                    });//end firebae ref
+                      
+                      if(count==0){ //meaning it never found a child and thus never transitioned to a map
+                         goToError();   
+                        }
+
+                  }//end auth else
+            }, {
+              remember: "sessionOnly"//delete user at broser close
+            });
+            
+
+        }//end QS else
+
+});//end doc ready
