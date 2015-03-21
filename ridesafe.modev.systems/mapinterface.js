fd8dadded4c787e602294a552628d499e0d8c271
jemrrs <jemrrs@gmail.com>
2015-03-21 03:00:18 -0500

    Final changes for authentication and scrip use.

--- ridesafe.modev.systems/mapinterface.js
+++ ridesafe.modev.systems/mapinterface.js
@@ -1,32 +1,42 @@
 $(document).ready(function(){
-        var map;  
-        var ref;
-    
-        function goToError(){
-            
-            setTimeout(function(){
-                $('body').addClass('fadeout');
-            }, 3000);
-            
-            var errorId = document.getElementById("error");
-            errorId.style.display = "block";
+    var map;  
+    var ref;
+    var token;
 
-            var mapId = document.getElementById("map-canvas");
-            mapId.style.display = "none"; 
-            
-            var splashId = document.getElementById("splash-wrapper");
-            slpashId.style.display = "none";
-            
-        }
-    
+    function goToError(){
 
+        setTimeout(function(){
+            $('body').addClass('fadeout');
+        }, 3000);
+
+        var errorId = document.getElementById("error");
+        errorId.style.display = "block";
+
+        var mapId = document.getElementById("map-canvas");
+        mapId.style.display = "none"; 
+    }
+    
+    function getToken(urlQS){
+        $.ajax({
+            url: "http://ridesafe.modev.systems/cgi-bin/makeAuthToken.py",
+            type: "post",
+            datatype: "text",
+            data: {"URL": urlQS},
+            success: function(scriptToken){
+                token=String(scriptToken);
+
+            }
+        })
+        
+    }
+    
     function initialize(){
-
         // Get a reference to our posts
-
         var myLatlng = null;
-        var prevLatlng = null;
-
+        var prevLatlng = null
+        var marker = null;
+        var firstMarker = null;
+        
         map = new google.maps.Map(document.getElementById('map-canvas'), {center: myLatlng, zoom: 17});
 
             ref.child(queryString).on("child_added", function(snapshot) {
@@ -40,42 +50,60 @@ $(document).ready(function(){
             //connect the dots
             prevLatlng = myLatlng;
             myLatlng = new google.maps.LatLng(lat,lng);
-
+            
+            //set begining point
+            if(prevLatlng==null && myLatlng!=null){
+                    firstMarker = new google.maps.Marker({
+                    position: myLatlng,
+                    title:"Starting Position"
+                });
+                firstMarker.setMap(map);
+            }
+                
             // Add to poly line and update/re-center Map
-            if(prevLatlng!=null&&myLatlng!=null){
+            if(prevLatlng!=null&&myLatlng!=null&&prevLatlng&&prevLatlng!=myLatlng){
+                
                 var flightPath = new google.maps.Polyline({
                 path: [prevLatlng,myLatlng],
                 geodesic: true,
-                strokeColor: '#003366',
+                strokeColor: '#D916B0',
                 strokeOpacity: 1.0,
-                strokeWeight: 2
+                strokeWeight: 10
               });
-
+                //remove old marker and add a new one for the current local
+                
+                if(marker!=null){
+                     marker.setMap(null);
+                }
+                
+                marker = new google.maps.Marker({
+                    position: myLatlng,
+                    title:"Last Recorded Position"
+                });
+                marker.setMap(map);
+                
+               
+                
                 flightPath.setMap(map);
                 map.setCenter(myLatlng);
-
-            }
+            }//end null if
 
             });
         
         }//end init
-        
-        //queryStrig extraction and firebase child check
-        var queryString = window.location.search;
-        queryString = queryString.substring(1);
-        if(queryString.length == 0){ 
-            goToError();
-        }
-        else{
+    //get tokens from makeAuthToken.py
+     
+    //Create firebase reference, start Auth
+    function construct(){
             ref = new Firebase("https://ridesafe.firebaseio.com/");
-            
-            ref.authAnonymously(function(error, authData) { 
+        
+            ref.authWithCustomToken(token, function(error, authData) { 
                  if (error) {
-                     window.alert("Authentication Error");
                      goToError();
                   } else {
+                    ref = new Firebase("https://ridesafe.firebaseio.com/");
                     var count=0;
-                      
+                           
                     ref.child(queryString).once('value', function(snapshot) {
                     count++;
                     if (snapshot.val() === null) {
@@ -83,24 +111,37 @@ $(document).ready(function(){
                     } 
                     else {
                         //if we got a good child, create a map and load it up with points, fadeout of loading screen
-
                            google.maps.event.addDomListener(window, 'load', initialize());
 
-                           google.maps.event.addListenerOnce(map, 'tilesloaded', function(){ $('body').addClass('fadeout'); });
+                           google.maps.event.addListenerOnce(map, 'tilesloaded', function(){$('body').addClass('fadeout'); });
                     }//end else
 
                     });//end firebae ref
                       
-                      if(count==0){ //meaning it never found a child and thus never transitioned to a map
-                         goToError();   
-                        }
+                      //if(count==0){ //meaning it never found a child and thus never transitioned to a map
+                    //     goToError();   
+                    //    }
 
                   }//end auth else
             }, {
-              remember: "sessionOnly"//delete user at broser close
+              remember: "sessionOnly"//delete user at browser close
             });
-            
-
-        }//end QS else
-
+        
+    }
+    
+        //queryStrig extraction and firebase child check
+        var queryString = window.location.search;
+        queryString = queryString.substring(1);
+        if(queryString.length == 0){ 
+            goToError();
+        }
+        else{
+            //get tokens from makeAuthToken.py, then go to constructor 
+            getToken(queryString);
+            setTimeout(function(){
+                construct();
+                
+            }, 3000);
+        }
+    
 });//end doc ready
